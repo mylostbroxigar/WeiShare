5f03df767af04ea24729ef5ad3acc6cbecbedc17
mylostbroxigar <jjjjoey@163.com>
2017-08-10 15:17:20 +0800

    update

--- app/src/main/java/com/borui/weishare/fragment/AccountFragment.java
+++ app/src/main/java/com/borui/weishare/fragment/AccountFragment.java
@@ -6,8 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.borui.weishare.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 /**
  * Created by borui on 2017/6/29.
@@ -15,10 +22,45 @@ import com.borui.weishare.R;
 
 public class AccountFragment extends Fragment {
 
+    @BindView(R.id.tv_curr_account)
+    TextView tvCurrAccount;
+    @BindView(R.id.tv_year_account)
+    TextView tvYearAccount;
+    @BindView(R.id.tv_details)
+    TextView tvDetails;
+    @BindView(R.id.tv_cash)
+    TextView tvCash;
+    @BindView(R.id.tv_card_type)
+    TextView tvCardType;
+    @BindView(R.id.tv_set_paypass)
+    TextView tvSetPaypass;
+    @BindView(R.id.layout_bind)
+    LinearLayout layoutBind;
+    Unbinder unbinder;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_account,null);
+        View view = inflater.inflate(R.layout.fragment_account, null);
+        unbinder = ButterKnife.bind(this, view);
         return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @OnClick({R.id.tv_details, R.id.tv_cash, R.id.layout_bind})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.tv_details:
+                break;
+            case R.id.tv_cash:
+                break;
+            case R.id.layout_bind:
+                break;
+        }
     }
 }
