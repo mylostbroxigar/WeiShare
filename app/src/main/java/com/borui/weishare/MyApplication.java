4d6d8b53302651d95a1d315c17e9fc13b8abf6a7
mylostpandora <jjjjoey@163.com>
2017-10-25 22:19:29 +0800

    update

--- app/src/main/java/com/borui/weishare/MyApplication.java
+++ app/src/main/java/com/borui/weishare/MyApplication.java
@@ -9,6 +9,8 @@ import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationListener;
 import com.borui.weishare.net.VolleyUtil;
 
+import cn.jpush.android.api.JPushInterface;
+
 
 /**
  * Created by borui on 2017/6/29.
@@ -28,7 +30,22 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        initVolley();
+        initJpush();
+        initAmap();
+    }
+
+
+    private void initVolley(){
         VolleyUtil.getInstance().init(this);
+    }
+
+    private void initJpush(){
+
+        JPushInterface.setDebugMode(true); 	// 设置开启日志,发布时请关闭日志
+        JPushInterface.init(this);
+    }
+    private void initAmap(){
         //初始化定位
         mLocationClient = new AMapLocationClient(getApplicationContext());
         mLocationListener = new AMapLocationListener() {
@@ -44,18 +61,17 @@ public class MyApplication extends Application {
                 }
             }
         };
-    //设置定位回调监听
+        //设置定位回调监听
         mLocationClient.setLocationListener(mLocationListener);
         mLocationOption = new AMapLocationClientOption();
         //设置定位模式为AMapLocationMode.Hight_Accuracy，高精度模式。
         mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
         mLocationOption.setOnceLocation(true);
         mLocationOption.setNeedAddress(true);
-    //启动定位
+        //启动定位
         mLocationClient.setLocationOption(mLocationOption);
         mLocationClient.startLocation();
 
     }
-
 
 }
